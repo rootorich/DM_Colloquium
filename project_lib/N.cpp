063eb1e0fcdf92520a3855fbaadd416e0be5df59
Xolozop <50333438+Xolozop@users.noreply.github.com>
2024-10-31 00:57:46 +0300

    add N-7

--- project_lib/N.cpp
+++ project_lib/N.cpp
@@ -1,42 +1,17 @@
 #include "N.h"
 
-test Test2(const test& t) {
-  test result;
-
-  for (size_t i = 0; i < t.test.size(); ++i) {
-    if (t.test[i] >= t.test[2]) {
-      result.test.emplace_back(t.test[i]);
-    } else {
-      result.test.emplace_back(t.test[3] + t.test[1] * 5);
-    }
-  }
-
-  return result;
+// MUL_Nk_N
+N operator<<(N num, const uint8_t k) {
+    if (num != 0)    // if num equal to zero don't change it
+        num.digits.resize(num.digits.size()+k);
+    return num;
 }
 
-void Test() {
-  test t;
+void operator<<=(N& num, const uint8_t k) {
+    if (num != 0)
+        num.digits.resize(num.digits.size()+k);
+}
 
-  std::vector<size_t> in = {1,2,3,4};
-
-  for (size_t i = 0; i < in.size(); ++i) {
-    t.test.emplace_back(in[i]);
-  }
-
-  for (auto el : in) {
-    t.test.emplace_back(el);
-  }
-
-  test t2 = Test2(t);
-
-  std::cout << "num like uint8_t unreadable\n";
-  std::cout << "but it is usable\n";
-  std::cout << "For example\n\n";
-  for (auto el : t2.test) {
-    std::cout << "size is " << sizeof(el) << "\n";
-    std::cout << "value is " << (size_t)el << "\n";
-    el = el * 2;
-    std::cout << "value after change " << (size_t)el << "\n";
-    std::cout << "size if use size_t " << sizeof((size_t)el) << "\n\n";
-  }
+N MUL_Nk_N(N num, const uint8_t k) {
+    return num<<k;
 }
