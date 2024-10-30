a6dcf686c7f1caa080be9d74c4fc2e351d35b8ec
Da-anil <danil33814@gmail.com>
2024-10-30 23:27:04 +0300

    N_1

--- project_lib/N.cpp
+++ project_lib/N.cpp
@@ -1,22 +1,41 @@
 #include "N.h"
 
 uint8_t COM_NN_D(const N &n1, const N &n2) {
-  if (n1.digits.size() > n2.digits.size()) {
+  if (n1 < n2) {
+    return 1; 
+  }
+  if (n2 < n1) {
     return 2;
-  }
-  if (n1.digits.size() < n2.digits.size()) {
-    return 1;
-  }
-  for (size_t i = n1.digits.size() - 1; i >= 0; i--) {
-    if (n1.digits[i] > n2.digits[i]) {
-      return 2;
-    }
-    if (n1.digits[i] < n2.digits[i]) {
-      return 1;
-    }
   }
   return 0;
 }
 
-bool operator==(const N &n1, const N &n2) { return COM_NN_D(n1, n2) == 0; }
-bool operator<(const N &n1, const N &n2) { return COM_NN_D(n1, n2) == 1; }
+// Тут можно просто return vec == vec, но пусть будет так 
+bool operator==(const N &n1, const N &n2) {   
+  if (n1.digits.size() != n2.digits.size()) {
+    return false;
+  }
+  for (size_t i = 0; i < n1.digits.size(); ++i) {
+    if (n1.digits[i] != n2.digits[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+bool operator<(const N &n1, const N &n2) { 
+  if (n1.digits.size() > n2.digits.size()) {
+    return false;
+  }
+  if (n1.digits.size() < n2.digits.size()) {
+    return true;
+  }
+  for (size_t i = n1.digits.size() - 1; i >= 0; --i) {
+    if (n1.digits[i] > n2.digits[i]) {
+      return false;
+    }
+    if (n1.digits[i] < n2.digits[i]) {
+      return true;
+    }
+  }
+  return false;
+}
