846fec08203e778faa698b7d8999b5ebfbe50f74
meowtyan228 <meowtyan228@yandex.ru>
2024-10-31 21:50:35 +0300

    MUL_ND_N: fixed

--- project_lib/N.cpp
+++ project_lib/N.cpp
@@ -31,43 +31,22 @@ bool NZER_N_B(const N& num) {
 
 N operator*(const N& num, const uint8_t digit){
     N result;
-    result.digits.resize(num.digits.size() + 1);
-    int carry = 0;
+    uint8_t carry = 0;
     for (size_t i = 0; i < num.digits.size(); ++i){
         int product = (num.digits[i] * digit)+carry;
-        result.digits[i] = product % 10;
+        result.digits.emplace_back(product%10);
         carry = product / 10;
     }
 
     if (carry > 0) {
-        result.digits[num.digits.size()] = carry;
-    }
-    else{
-        result.digits.resize(num.digits.size());
+        result.digits.emplace_back(carry);
     }
     return result;
 }
 
 N operator*(const uint8_t digit, const N& num){
-    N result;
-    result.digits.resize(num.digits.size() + 1);
-    int carry = 0;
-    for (size_t i = 0; i < num.digits.size(); ++i){
-        int product = (num.digits[i] * digit)+carry;
-        result.digits[i] = product % 10;
-        carry = product / 10;
-    }
-
-    if (carry > 0) {
-        result.digits[num.digits.size()] = carry;
-    }
-    else{
-        result.digits.resize(num.digits.size());
-    }
-    return result;
+    return num*digit;
 }
-
-
 
 N MUL_ND_N(const N& num, const uint8_t digit){
     return num*digit;
