4db9f83a6d23bd98bc82894c40bb2fef357e6d67
rootorich <rootorich@gmail.com>
2024-11-01 21:14:43 +0300

    update N (N-3, N-4, N-5, N10, N-14)

--- project_lib/N.cpp
+++ project_lib/N.cpp
@@ -1,5 +1,7 @@
 #include "N.h"
 
+using namespace std::rel_ops;
+
 /*
  * Efimova
  * N-2
@@ -48,6 +50,41 @@ N operator*(const uint8_t digit, const N& num){
 
 N MUL_ND_N(const N& num, const uint8_t digit){
   return num * digit;
+}
+
+/*
+ * N-14
+*/
+
+N operator/(const N& num1, const N& num2) {
+
+//  if (num2 == 0) {
+//    throw std::invalid_argument("Division by zero");
+//  }
+
+  N n1 = num1;
+  N res;
+
+  if (n1.digits < num2.digits) res.digits.push_back(0);
+
+  while(n1.digits >= num2.digits){
+    uint8_t tmp = DIV_NN_Dk(n1, num2);
+    res.digits.push_back(tmp);
+    N product = num2*tmp;
+    uint8_t t = n1.digits.size() - product.digits.size();
+    n1 = n1 - (product << t);
+  }
+
+  return res;
+
+}
+
+N operator%(const N& num1, const N& num2) {
+  return num1 - (num1/num2);
+}
+
+N LCM_NN_N(const N& num1, const N& num2){
+  return MUL_NN_N(num1, num2)/GCF_NN_N(num1, num2);
 }
 
 /*
@@ -102,6 +139,89 @@ uint8_t COM_NN_D(const N &n1, const N &n2) {
 }
 
 /*
+* N-5
+*/
+
+N operator-(const N& n1, const N& n2) {
+  if (n1 < n2) {
+    return N{{0}};
+  }
+
+  N result;
+  result.digits = n1.digits;
+  int carry = 0;
+  size_t i = 0;
+
+  for (i = 0; i < n2.digits.size(); ++i) {
+    if (n1.digits[i] - carry < n2.digits[i]) {
+      result.digits[i] = 10 + n1.digits[i] - carry - n2.digits[i];
+      carry = 1;
+    }
+    else {
+      result.digits[i] = n1.digits[i] - carry - n2.digits[i];
+      carry = 0;
+    }
+  }
+
+  while (carry && i < n1.digits.size()) {
+    if (n1.digits[i] == 0) {
+      result.digits[i] = 10 + n1.digits[i] - carry;
+      carry = 1;
+    }
+    else {
+      result.digits[i] = n1.digits[i] - carry;
+      carry = 0;
+    }
+    ++i;
+  }
+
+  while (result.digits.size() > 1 && result.digits.back() == 0) {
+    result.digits.pop_back();
+  }
+  return result;
+}
+
+N SUB_NN_N(const N& n1, const N& n2) {
+  if (n1 < n2) {
+    return n2 - n1;
+  }
+  return n1 - n2;
+}
+
+/*
+ * N-10
+*/
+
+// Не тестил пока сложения нет
+N DIV_NN_Dk(const N& n1, const N& n2) {
+  N a, b;
+  if (n2 < n1) {
+    a.digits = n1.digits;
+    b.digits = n2.digits;
+  }
+  else {
+    a.digits = n2.digits;
+    b.digits = n1.digits;
+  }
+  int k = a.digits.size() - b.digits.size();
+  uint8_t D;
+  if (a.digits.back() > b.digits.back()) {
+    D = a.digits.back() / b.digits.back();
+    return N{{D}} << k;
+  }
+  k -= 1;
+  b = b << k;
+  N sum;
+  sum.digits = b.digits;
+  uint8_t i = 0;
+  while (a > sum) {
+    sum = sum + b;
+    ++i;
+  }
+  return N{{i}} << k;
+}
+
+/*
  * End Savranraskii Danila
 */
 
@@ -109,6 +229,56 @@ uint8_t COM_NN_D(const N &n1, const N &n2) {
 
 /*
  * Masha
+ * N-4
+*/
+N operator+(const N& n1, N n2) {
+  N result = n1;
+  if (COM_NN_D(n1, n2) == 1) {
+    result = n2;
+    n2 = n1;
+  }
+  uint8_t carry = 0;
+  size_t i = 0;
+  while (i < n2.digits.size() || carry) {
+    if (i == result.digits.size()) {
+      result.digits.emplace_back(carry);
+      break;
+    }
+    uint8_t digit = result.digits[i] + carry;
+    if (i < n2.digits.size()) {
+      digit += n2.digits[i];
+    }
+    result.digits[i] = digit%10;
+    carry = digit/10;
+    ++i;
+  }
+  return result;
+}
+
+N& operator+=(N& n1, const N& n2) {
+  n1 = n1 + n2;
+  return n1;
+}
+
+N ADD_NN_N(const N& n1, const N& n2) {
+  return n1 + n2;
+}
+
+
+/*
+ * N-3
+*/
+const N& operator++(N& num) {
+  N num_one{std::vector<uint8_t>{1}};
+  num += num_one;
+  return num;
+}
+
+const N ADD_1N_N(N num) {
+  return ++num;
+}
+
+/*
  * N-7
 */
 
