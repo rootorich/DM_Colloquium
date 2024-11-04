205df9438f4cc073a887d2da78e153982f22ec66
rootorich <rootorich@gmail.com>
2024-11-04 19:06:41 +0300

    add P (P-1, P-2, P-3)

--- project_lib/P.cpp
+++ project_lib/P.cpp
@@ -1,1 +1,64 @@
 #include "P.h"
+
+/*
+ * Efimova
+ * P-1
+*/
+P operator+(const P& p1, const P& p2) {
+  P result = (p1.a.size() >= p2.a.size()) ? p1 : p2;
+  P min = (p1.a.size() < p2.a.size()) ? p1: p2;
+
+  for (size_t i = 0; i < min.a.size(); ++i) {
+    result.a[i] = result.a[i] + min.a[i];
+  }
+
+  return result;
+}
+
+P ADD_PP_P(const P& p1, const P& p2) {
+  return p1 + p2;
+}
+
+/*
+ * P-2
+*/
+P operator-(const P& p1) {
+  P p = p1;
+
+  for (auto & a : p.a){
+    a = -a;
+  }
+
+  return p;
+}
+
+P operator-(const P& p1, const P& p2) {
+  return p1 + (-p2);
+}
+
+P SUB_PP_P(const P& p1, const P& p2) {
+  return p1 - p2;
+}
+
+/*
+ * P-3
+*/
+P operator*(const P& p, const Q& q) {
+  P result;
+
+  for (const auto & a : p.a){
+    result.a.push_back(a * q);
+  }
+
+  return result;
+}
+
+P MUL_PQ_P(const P& p, const Q& q) {
+  return p * q;
+}
+
+
+
+/*
+ * End Efimova
+*/
--- project_lib/P.h
+++ project_lib/P.h
@@ -3,4 +3,33 @@
 #ifndef DM_COLLOQUIUM_PROJECT_LIB_P_H_
 #define DM_COLLOQUIUM_PROJECT_LIB_P_H_
 
+struct P {
+  std::vector<Q> a;
+};
+
+/*
+ * Efimova
+ * P-1
+*/
+P operator+(const P& p1, const P& p2);
+P ADD_PP_P(const P& p1, const P& p2);
+
+/*
+ * P-2
+*/
+P operator-(const P& p1);
+P operator-(const P& p1, const P& p2);
+P SUB_PP_P(const P& p1, const P& p2);
+
+/*
+ * P-3
+*/
+P operator*(const P& p, const Q& q);
+P MUL_PQ_P(const P& p, const Q& q);
+
+
+/*
+ * End Efimova
+*/
+
 #endif
