d8f5f32170137e99ac08a37b379937193df8b211
rootorich <rootorich@gmail.com>
2024-11-13 14:55:42 +0300

    update P (P-4, P-5, P-6, P-7)

--- project_lib/P.cpp
+++ project_lib/P.cpp
@@ -57,8 +57,67 @@ P MUL_PQ_P(const P& p, const Q& q) {
   return p * q;
 }
 
+/*
+ * End Efimova
+*/
 
 
 /*
- * End Efimova
+* Savranraskii Danila
+* P-4
+*/
+P MUL_Pxk_P(const P &p, uint32_t k) {
+  P result = p;
+
+  Q zero = {Z("0"), N("1")};
+  result.a.insert(result.a.begin(), k, zero);
+
+  return result;
+}
+
+/*
+ * P-5
+*/
+Q LED_P_Q(const P &p) {
+  return p.a.back();
+}
+
+/*
+ * P-6
+*/
+N DEG_P_N(const P &p) {
+  return N(p.a.size() - 1);
+}
+
+/*
+ * P-7
+*/
+Q FAC_P_Q(const P &p) {
+  N GCF = N(1); // НОД
+  N LCM = N(1); // НОК
+
+  size_t i = 0;
+  // Находим первую значащую цифру и приписываем ей НОК и НОД
+  while (i < p.a.size()) {
+    if (p.a[i].z != 0) {
+      GCF = p.a[i].z;
+      LCM = p.a[i].n;
+      ++i;
+      break;
+    }
+    ++i;
+  }
+  // Учитываем оставшиеся числа
+  while (i < p.a.size()) {
+    if (p.a[i].z != 0) {
+      GCF = GCF_NN_N(GCF, p.a[i].z); // НОД всех числителей
+      LCM = LCM_NN_N(LCM, p.a[i].n); // НОК всех знаменателей
+    }
+    ++i;
+  }
+  return Q{TRANS_N_Z(GCF), LCM};
+}
+
+/*
+ * End Savranraskii Danila
 */
--- project_lib/P.h
+++ project_lib/P.h
@@ -27,9 +27,34 @@ P SUB_PP_P(const P& p1, const P& p2);
 P operator*(const P& p, const Q& q);
 P MUL_PQ_P(const P& p, const Q& q);
 
-
 /*
  * End Efimova
+*/
+
+
+/*
+* Savranraskii Danila
+* P-4
+*/
+P MUL_Pxk_P(const P &p, uint32_t k);
+
+/*
+ * P-5
+*/
+Q LED_P_Q(const P &p);
+
+/*
+ * P-6
+*/
+N DEG_P_N(const P &p);
+
+/*
+ * P-7
+*/
+Q FAC_P_Q(const P &p);
+
+/*
+ * End Savranraskii Danila
 */
 
 #endif
