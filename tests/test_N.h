264857ddd8f945b37e105de970c3326b10910d28
Xolozop <50333438+Xolozop@users.noreply.github.com>
2024-12-24 12:54:54 +0300

    Add files via upload

--- tests/test_N.h
+++ tests/test_N.h
@@ -1,6 +1,6 @@
 #include <iostream>
 
-#include "../project_lib/N.h"
+#include "N.h"
 
 #ifndef DM_COLLOQUIUM_TESTS_TEST_N_H_
 #define DM_COLLOQUIUM_TESTS_TEST_N_H_
@@ -14,9 +14,18 @@ bool TEST_F_NZER_N_B();
 bool TEST_OP_EQU_N_N();
 bool TEST_OP_LT_N_N();
 bool TEST_F_COM_NN_D();
-
-
+bool TEST_ADD_1N_N();
+bool TEST_ADD_NN_N();
+bool TEST_SUB_NN_N();
+bool TEST_MUL_ND_N();
+bool TEST_MUL_Nk_N();
+bool TEST_MUL_NN_N();
+bool TEST_SUB_NDN_N();
+bool TEST_DIV_NN_Dk();
+bool TEST_DIV_NN_N();
+bool TEST_MOD_NN_N();
+bool TEST_GCF_NN_N();
+bool TEST_LCM_NN_N();
 void ALL_TEST_N();
-
 
 #endif
